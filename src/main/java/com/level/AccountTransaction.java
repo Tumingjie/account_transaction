67a43a8c0f0b81eb33e749cd40c243536ed91adf
tumingjie <1019901646@qq.com>
2023-08-15 17:11:28 +0800

    换名

--- src/main/java/com/level/AccountTransaction.java
+++ src/main/java/com/level/AccountTransaction.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.level.**"})
-public class QueueToCallApplication {
+public class AccountTransaction {
 
     public static void main(String[] args) {
         //改变rocketMQ客户端的日志输出位置为当前程序的logs目录下
         System.setProperty("rocketmq.client.logRoot","logs/");
-        SpringApplication.run(QueueToCallApplication.class, args);
+        SpringApplication.run(AccountTransaction.class, args);
     }
 
 }
--- src/test/java/com/level/queuetocall/AccountTransactionTests.java
+++ src/test/java/com/level/queuetocall/AccountTransactionTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QueueToCallApplicationTests {
+class AccountTransactionTests {
 
     @Test
     void contextLoads() {
